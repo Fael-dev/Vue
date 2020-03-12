c746013d6735b412784306fbc0fc719fa1bd2530
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-12 13:17:35 -0300

    Aula05 - Adicionando comportamento ao componente ao clicar no titulo

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,48 +1,57 @@
 <template>
   <div class="painel">
-    <h2 class="painel-titulo">{{ titulo }}</h2>
-    <slot class="painel-conteudo">
-      <!--
-        SLOT FAZ QUASE A MSM COISA QUE UM BLOCK NO DJANGO
-        <slot name="header"> CHAMA O SLOT PELO NOME
-        <div slot="header"></div>
-       -->
-
-    </slot>
+    <h2 class="painel-titulo" @click="visivel = !visivel">{{ titulo }}</h2>
+    <div class="painel-conteudo" v-show="visivel">
+      <slot>
+        <!--
+          SLOT FAZ QUASE A MSM COISA QUE UM BLOCK NO DJANGO
+          <slot name="header"> CHAMA O SLOT PELO NOME
+          <div slot="header"></div>
+          SLOT não recebe diretivas
+          DIRETIVA v-on: => forma simples @
+         -->
+      </slot>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    props:['titulo']
+    props: ['titulo'],
+    data(){
+      return{
+        visivel:true
+      }
+    }
   }
 </script>
 
 <style scoped> // SCOPED quer dizer que esse estilo só pega nesse componente
-  /* estilo do painel */
+/* estilo do painel */
 
-  .painel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
 
-  .painel .painel-titulo {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
-  * {
-    box-shadow:5px 5px 5px;
-  }
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+
+* {
+  box-shadow: 5px 5px 5px;
+}
 
 </style>
