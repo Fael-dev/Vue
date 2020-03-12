8bca7b731ce24bbf92aa408197ecb247a1075b52
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-12 09:18:26 -0300

    Aula02 - Utilizando diretivas

--- src/main.js
+++ src/main.js
@@ -5,3 +5,14 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+/*
+ACIMA ESTÁ EM ECMASCRIPT 6 - 2015
+MESMA REPRESENTACAO DA DECLARACAO ACIMA
+new Vue({
+  el: '#app',
+  render: function () {
+    h(App);
+  }
+})
+*/
