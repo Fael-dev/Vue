db6b938c1747e25a9087ff326b52f581acac7c29
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-12 12:00:08 -0300

    Aula05 - Adicionando a logica de filtrar

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Pesquise por títulos...">
     {{ filtro }}
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
         <meu-Painel :titulo="foto.titulo">
         <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
         </meu-Painel>
@@ -25,6 +25,16 @@
         titulo: "Alurapic",
         fotos: [],
         filtro: ''
+      }
+    },
+    computed:{
+      fotosComFiltro(){
+        if(this.filtro) {
+          let exp = new RegExp(this.filtro.trim(), 'i');
+          return this.fotos.filter(foto => exp.test(foto.titulo));
+        }else{
+          return this.fotos;
+        }
       }
     },
     created() {
