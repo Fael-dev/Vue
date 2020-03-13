18290b14bd96b3c20ea9ff56b3d67d7b5838090d
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-13 16:11:32 -0300

    (VUE2) Aula02 - Criando um modelo

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -36,6 +36,7 @@
 
   import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
   import Botao from '../shared/botao/Botao.vue';
+  import Foto from "../../domain/foto/Foto";
 
   export default {
 
@@ -46,22 +47,14 @@
     },
     data() {
       return {
-        foto: {
-          titulo: '',
-          url: '',
-          descricao: ''
-        }
+        foto: new Foto()
       }
     },
     methods: {
       grava() {
         console.log("Enviar para o servidor");
         console.log(this.foto);
-        this.foto = {
-          titulo: '',
-          url: '',
-          descricao: ''
-        }
+        this.foto = new Foto;
       }
     }
   }
