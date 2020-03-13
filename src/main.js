4d020d0f831ee9adac4ddb0bad7d5971f362e387
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-13 09:22:06 -0300

    Aula06 - Configurando rotas*

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ Vue.use(VueResource);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes:routes // PODE-SE DEIXAR APENAS routes, SEGUNDO O ECMASCRIPT 6, QUANDO O VALOR TEM O MSM NOME DA PROPRIEDADE
+  routes:routes, // PODE-SE DEIXAR APENAS routes, SEGUNDO O ECMASCRIPT 6, QUANDO O VALOR TEM O MSM NOME DA PROPRIEDADE
+  mode: 'history' // MUDA O PADRÃO DE: LOCALHOST:8080/#/ PARA LOCALHOST:8080/
 });
 
 new Vue({
