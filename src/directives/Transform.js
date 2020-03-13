58317eb10db8a068aff1aae84cbd47150e45c62f
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-13 14:14:36 -0300

    (VUE2) Aula01 - Adicionando modificadores a diretiva

--- src/directives/Transform.js
+++ src/directives/Transform.js
@@ -5,17 +5,17 @@ Vue.directive('meu-transform', {
     let current = 0;
     el.addEventListener('click',function(){
       //let incremento = binding.value || 90; // Pega o valor passado, se não tiver valor, vai ser 90
-      let incremento = 90;
-      let animate = false;
-      if(binding.value){
-        incremento = binding.value.incremento;
-        animate = binding.value.animate;
+      let incremento = binding.value || 90;
+
+      if(binding.modifiers.reverse){
+        current -= incremento;
+      }else{
+        current += incremento;
       }
-      current += incremento;
       el.style.transition = `transform 0.5s`;
       el.style.transform = `rotate(${current}deg)`;
 
-      if(animate) el.style.transition = 'transform 2s';
+      if(binding.modifiers.animate) el.style.transition = 'transform 2s';
     });
   }
 });
