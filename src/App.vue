720be575971a4f3a390e2d8de885c7b85aaac600
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-13 10:07:58 -0300

    Aula06 - Transformando o menu em um componente

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="corpo">
-    <nav>
-      <ul>
-        <li v-for="route in routes"><router-link :to="route.path">{{ route.nome }}</router-link></li>
-        <!-- <li v-for="route in routes"><router-link :to="route.path ? route.path : '/' ">{{ route.nome }}</router-link></li> -->
-      </ul>
-    </nav>
+    <meu-Menu :rotas="routes"></meu-Menu>
     <transition name="pagina">
       <router-view></router-view>
     </transition>
@@ -17,9 +12,13 @@
 
 <script>
   import { routes } from './routes';
+  import Menu from './components/shared/menu/Menu.vue';
   export default {
-    data(){
-      return{
+    components:{
+      'meu-Menu': Menu
+    },
+    data() {
+      return {
         routes
       }
     }
@@ -32,10 +31,12 @@
     width: 96%;
     margin: 0 auto;
   }
-  .pagina-enter, .pagina-leave-active{
-    opacity:0;
+
+  .pagina-enter, .pagina-leave-active {
+    opacity: 0;
   }
-  .pagina-enter-active, .pagina-leave-active{
+
+  .pagina-enter-active, .pagina-leave-active {
     transition: opacity .4s;
   }
 </style>
---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav>
+    <ul>
+      <li v-for="rota in rotas">
+        <router-link :to="rota.path">{{ rota.nome }}</router-link>
+      </li>
+      <!-- <li v-for="rota in rotas"><router-link :to="rota.path ? rota.path : '/' ">{{ rota.nome }}</router-link></li> -->
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: "Menu",
+    props: {
+      rotas: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
