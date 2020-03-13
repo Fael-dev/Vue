2459352ed03f0189c5ed760fc7fd5747df86d179
Rafael <rafael@MacBook-Pro-de-FIXPAY.local>
2020-03-13 09:47:33 -0300

    Aula06 - Adicionando animacoes nas transicoes de pagina

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,9 @@
         <!-- <li v-for="route in routes"><router-link :to="route.path ? route.path : '/' ">{{ route.nome }}</router-link></li> -->
       </ul>
     </nav>
-    <router-view></router-view>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
   </div>
   <!--
     NÃO PODE USAR A TAG a:href POIS VAI RECARREGAR A PÁGINA E SPA(SIMPLE PAGE APPLICATION), NÃO PODE SER RECARREGADA
@@ -29,5 +31,11 @@
     font-family: Helvetica, sans-serif;
     width: 96%;
     margin: 0 auto;
+  }
+  .pagina-enter, .pagina-leave-active{
+    opacity:0;
+  }
+  .pagina-enter-active, .pagina-leave-active{
+    transition: opacity .4s;
   }
 </style>
